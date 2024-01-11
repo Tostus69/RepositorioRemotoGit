47d75eb5864f1241cc6e82f0d28ba8084f480ef5
dam-m <dam-m@profe-HP-EliteDesk-800-G1-TWR>
2024-01-11 13:25:22 +0100

    Hola

--- Proyecto1/src/paquete1/Principal.java
+++ Proyecto1/src/paquete1/Principal.java
@@ -1,5 +1,9 @@
 package paquete1;
 
 public class Principal {
+	public static void main(String[] args) {
+		// Todo Auto-generated methos stub
+		System.out.println("Ejemplo de control de versiones con git");
+	}
 
 }
